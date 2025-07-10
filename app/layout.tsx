006e7527f58e8facd79fb4018fca027a27c61f63
da-nn-yy <db7394928@gmail.com>
2025-07-10 14:43:44 +0300

    modified the layout and global style sheet for better interface look

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Geist } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
